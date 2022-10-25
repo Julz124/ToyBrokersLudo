e06f1bff0a4b511757d4501c7eee880db7aa4f57
ThatsNotMe <mineflax@gmail.com>
2022-10-25 16:50:15 +0200

    NewProjectUpload
    Setting up the whole new project

---
+++ build.sbt
@@ -0,0 +1,10 @@
+ThisBuild / version := "0.1.0-SNAPSHOT"
+
+ThisBuild / scalaVersion := "3.2.0"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "ToyBrokersLudo"
+  )
+libraryDependencies += "org.scalactic" %% "scalactic" % "3.2.14"
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.14" % "test"
---
+++ src/main/scala/de/htwg/se/toybrokersludo/model/field.scala
@@ -0,0 +1,33 @@
+package de.htwg.se.toybrokersludo.model
+
+class field {
+  val map: List[List[Boolean]] = List(
+    List(true, false, true, false, true, true, true, false, true, false, true),
+    List(false, false, false, false, true, true, true, false, false, false, false),
+    List(true, false, true, false, true, true, true, false, true, false, true),
+    List(false, false, false, false, true, true, true, false, false, false, false),
+    List(true, true, true, true, true, true, true, true, true, true, true),
+    List(true, true, true, true, true, false, true, true, true, true, true),
+    List(true, true, true, true, true, true, true, true, true, true, true),
+    List(false, false, false, false, true, true, true, false, false, false, false),
+    List(true, false, true, false, true, true, true, false, true, false, true),
+    List(false, false, false, false, true, true, true, false, false, false, false),
+    List(true, false, true, false, true, true, true, false, true, false, true))
+
+  val eol: String = "\n"
+
+  def horizontal(list: List[Boolean], size: Int): List[String] =
+    list.map((b: Boolean) => if (b) "+" + "-" * size + "+" else " " + " " * size + " ")
+
+  def vertical(list: List[Boolean], size: Int): List[String] =
+    list.map((b: Boolean) => if (b) "|" + " " * size + "|" else " " + " " * size + " ")
+
+  def mash(map: List[List[Boolean]] = map, size: Int = 4): List[String] =
+    for (list <- map) yield horizontal(list, size).mkString + eol + vertical(list, size).mkString + eol + horizontal(list, size).mkString + eol
+
+  def toString2(): String =
+    mash().mkString
+
+
+  print(toString2())
+}
