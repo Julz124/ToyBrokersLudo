092594e55ee62a690487ab47e94227769ec7b24a
Felix Fischer <felixarjunafischer@gmx.de>
2022-11-03 20:58:58 +0100

    bug fix

--- src/test/scala/de/htwg/se/toybrokersludo/model/PlayerSpec.scala
+++ src/test/scala/de/htwg/se/toybrokersludo/model/PlayerSpec.scala
@@ -4,15 +4,11 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 class PlayerSpec extends AnyWordSpec with Matchers {
-
   "player" should {
-
     "have a number and color" in {
       val player = Player(0,"Y")
       player.number should be (0)
       player.color should be ("Y")
     }
-
   }
-
 }
