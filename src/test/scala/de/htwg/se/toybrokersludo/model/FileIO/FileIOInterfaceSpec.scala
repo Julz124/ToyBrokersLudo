4e3788e8b95b040b44211cbc38c86b624295ce1f
ThatsNotMe <mineflax@gmail.com>
2023-01-13 12:56:02 +0100

    FileIO tests
    -

--- src/test/scala/de/htwg/se/toybrokersludo/model/FileIO/FileIOInterfaceSpec.scala
+++ src/test/scala/de/htwg/se/toybrokersludo/model/FileIO/FileIOInterfaceSpec.scala
@@ -2,7 +2,8 @@ package de.htwg.se.toybrokersludo.model.FileIO
 
 import de.htwg.se.toybrokersludo.model.FileIO.FileIOInterface
 import de.htwg.se.toybrokersludo.model.FieldInterface
-
+import de.htwg.se.toybrokersludo.model.FieldBaseImpl.{Field, Matrix}
+import de.htwg.se.toybrokersludo.util.PlayerBaseImpl.{BluePlayer, GreenPlayer, RedPlayer, YellowPlayer}
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers.should
@@ -13,17 +14,15 @@ class FileIOInterfaceSpec extends AnyWordSpec with Matchers {
     import de.htwg.se.toybrokersludo.model.FileIO.JsonImpl.FileIo
     val jsonImpl: FileIOInterface = FileIo()
 
-    "can save" in {
-      val field: FieldInterface = Field(Matrix(),GreenPlayer,1)
-      jsonImpl.save()
-    }
-
     "can load" in {
-      jsonImpl.load()
+      val matrix: Matrix = Matrix()
+      val field: FieldInterface = Field(matrix,GreenPlayer,1)
+      jsonImpl.save(field, "SpecTestJson")
+      jsonImpl.load("SpecTestJson").toString should equal (Field(matrix,GreenPlayer,1).toString)
     }
 
     "can getTargets" in {
-      jsonImpl.getTargets()
+      jsonImpl.getTargets() should contain ("saveGameJson\\SpecTestJson")
     }
 
   }
@@ -31,6 +30,17 @@ class FileIOInterfaceSpec extends AnyWordSpec with Matchers {
   "xml" should {
     import de.htwg.se.toybrokersludo.model.FileIO.XmlImpl.FileIo
     val xmlImpl: FileIOInterface = FileIo()
+
+    "can load" in {
+      val matrix: Matrix = Matrix()
+      val field: FieldInterface = Field(matrix, GreenPlayer, 1)
+      xmlImpl.save(field, "SpecTestXml")
+      xmlImpl.load("SpecTestXml").toString should equal(Field(matrix, GreenPlayer, 1).toString)
+    }
+
+    "can getTargets" in {
+      xmlImpl.getTargets() should contain("saveGameXml\\SpecTestXml")
+    }
   }
 
 
