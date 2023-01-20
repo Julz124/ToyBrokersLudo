97e6371cadb024040c313f237c40e229978887ce
Felix Fischer <felixarjunafischer@gmx.de>
2023-01-20 10:55:53 +0100

    refaktor

--- src/test/scala/de/htwg/se/toybrokersludo/controller/ControllerInterfaceSpec.scala
+++ src/test/scala/de/htwg/se/toybrokersludo/controller/ControllerInterfaceSpec.scala
@@ -392,7 +392,7 @@ class ControllerInterfaceSpec extends AnyWordSpec with Matchers {
             "+----+      +----+      +----++----++----+      +----+      +----+" + eol +
             "|    |      |    |      |    ||    ||    |      |    |      |    |" + eol +
             "+----+      +----+      +----++----++----+      +----+      +----+" + eol +
-            "Green Player have to dice"
+            "Green Player have to move"
         )
     }
 
