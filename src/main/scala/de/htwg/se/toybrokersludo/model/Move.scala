8b1c10f6018bc163dc8d7aa6107677de7ee02eee
FelixSubject211 <113304291+FelixSubject211@users.noreply.github.com>
2024-03-20 11:22:37 +0100

    DefaultControllerSpec

--- src/main/scala/de/htwg/se/toybrokersludo/model/Move.scala
+++ src/main/scala/de/htwg/se/toybrokersludo/model/Move.scala
@@ -3,9 +3,6 @@ package de.htwg.se.toybrokersludo.model
 import de.htwg.se.toybrokersludo.model.Cell
 
 case class Move(fromIndex: Int, toIndex: Int):
-  def fromCell(map: Map[(Int, Int), Cell]): Cell =
-    map.values.find( cell => cell.index == fromIndex ).get
-
   def toCell(map: Map[(Int, Int), Cell]): Cell =
     map.values.find( cell => cell.index == toIndex ).get
 
